e51201e6373b39f14562cd3994b6f7d4a8a0b3ef
RakanEngineer <rakan.engineer@gmail.com>
2025-10-23 23:15:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace C__FundamentalsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
