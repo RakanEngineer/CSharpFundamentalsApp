3958eb9c9d064ba85cb01d89478c99c30bebd583
RakanEngineer <rakan.engineer@gmail.com>
2025-10-24 00:03:50 +0200

    Fixed output formatting

--- Program.cs
+++ Program.cs
@@ -31,25 +31,24 @@
             Console.Write("Choose a door: 1, 2, or 3: ");
             string userValue = Console.ReadLine();
 
+            string message = "";
             if (userValue == "1")
             {
-                string message = "You won a new car!";
-                Console.WriteLine(message);
+                 message = "You won a new car!";
             }
             else if (userValue == "2")
             {
-                string message = "You won a new boat!";
-                Console.WriteLine(message);
+                 message = "You won a new boat!";
             }
             else if (userValue == "3")
             {
-                string message = "You won a new cat!";
-                Console.WriteLine(message);
+                 message = "You won a new cat!";
             }
             else
             {
                 Console.WriteLine("Sorry, we didn't understand!");
             }
+            Console.WriteLine(message);
             Console.ReadLine(); 
         }
     }
